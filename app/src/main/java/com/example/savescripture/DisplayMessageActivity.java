02fd6733b0a56e476ebad2ae529fec7d4bae613c
Oswaldo Rodriguez <Owner2303@gmail.com>
2021-10-16 14:08:27 -0400

    Used butter knife for the last two, and it was cool

--- app/src/main/java/com/example/savescripture/DisplayMessageActivity.java
+++ app/src/main/java/com/example/savescripture/DisplayMessageActivity.java
@@ -21,10 +21,19 @@ import butterknife.OnClick;
 
 public class DisplayMessageActivity extends AppCompatActivity {
     private static final String TAG = "DisplayMessageActivity";
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.addingBook) TextView tvReceivedData;
     //ButterKnife
     @SuppressLint("NonConstantResourceId")
     @OnClick(R.id.saveScripture) void submit(View view) {
-        Toast.makeText(this, "Works :D", Toast.LENGTH_SHORT).show();
+        SharedPreferences sharedPref = getSharedPreferences("session" ,Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+
+        editor.putString("SavedScripture", tvReceivedData.getText().toString()).apply();
+
+        Toast.makeText(this, "Scripture was saved successfully.", Toast.LENGTH_LONG ).show();
+
+        Toast.makeText(this, sharedPref.getString("SavedScripture", "error"), Toast.LENGTH_SHORT).show();
     }
 
     @Override
@@ -33,10 +42,6 @@ public class DisplayMessageActivity extends AppCompatActivity {
         setContentView(R.layout.activity_display_message);
         ButterKnife.bind(this);
         Bundle bundle = getIntent().getExtras();
-        TextView tvReceivedData  = (TextView) findViewById(R.id.addingBook);
-        
-        SharedPreferences sp = getSharedPreferences("MyPrefs", Context.MODE_PRIVATE);
-
 
         if (bundle != null){
             String book = bundle.getString("Book");
--- app/src/main/java/com/example/savescripture/MainActivity.java
+++ app/src/main/java/com/example/savescripture/MainActivity.java
@@ -1,18 +1,40 @@
 package com.example.savescripture;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    @SuppressLint("NonConstantResourceId")
+    @BindView(R.id.scriptureRetrieved) TextView scRetrieved;
+
+    @SuppressLint("NonConstantResourceId")
+    @OnClick(R.id.loadScripture) void loadScripture(View view) {
+        SharedPreferences sharedPref = getSharedPreferences("session" , Context.MODE_PRIVATE);
+        String result = sharedPref.getString("SavedScripture", "error");
+        scRetrieved.setText(result);
+        Toast.makeText(this,  result, Toast.LENGTH_SHORT).show();
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
     }
     //Called when the user taps the Send button
     //event triggered when the button is clicked
